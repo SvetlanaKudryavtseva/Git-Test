fd2ffd17e03f5946789362cd169b43a16af064fe
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-03-30 12:43:55 +0300

    JS Циклы

--- js-basics.js
+++ js-basics.js
@@ -1,4 +1,5 @@
 // egghead --- Learn ES6 (ECMAScript 2015) --- 01. arrow functions
+/*
 var createGreeting = function(message, name) {
     return message + name;
 }
@@ -19,3 +20,35 @@ console.log(arrowFunction__small__oneParameter('arrowYou___oneParameter'));
 
 // itgid.info --- JavaScript v. 2.0 --- 01. innerHTML
 document.querySelector('.inner').innerHTML = "Inner HTML";
+
+// itgid.info --- JavaScript v. 2.0 --- 06. Циклы
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+for (i = 0; i < arr.length; i++) {
+    console.log(arr[1])
+}
+
+let one = document.querySelector('.one')
+console.log(one);
+one.style.background = "red";
+let two = document.querySelectorAll('.two')
+console.log(two);
+for (i = 0; i < two.length; i++) {
+    console.log(two[i])
+    two[i].style.border = "1px solid blue";
+}
+let stars = document.querySelectorAll('.stars')
+console.log(stars);
+for (let i = 0; i < stars.length; i++) {
+    stars[i].innerHTML = '*****';
+}
+*/
+document.querySelector('.button-for-radio').onclick = () => {
+    let r = document.querySelectorAll('input[type="radio"]');
+    console.log(r);
+    for (i = 0; i < r.length; i++) {
+        if (r[i].checked) {
+            console.log(r[i].value);
+        }
+    }
+}
